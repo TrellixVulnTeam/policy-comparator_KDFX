8a931e2eedf2a34604b57a5f44969f14e90171a8
yanndav <yann.collindavid@gmail.com>
2021-02-11 16:19:34 +0100

    adding migration to repo

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # App initialisation
 from logging import makeLogRecord
-from flask import Flask
+from flask import Flask, app
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
